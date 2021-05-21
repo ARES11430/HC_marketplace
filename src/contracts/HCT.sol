a25e6a1c930c9433996b9c57eb100959ff1d5812
ARES11430 <amin11.ghasemi@gmail.com>
2021-05-21 18:40:25 +0430

    Update HCT.sol

--- src/contracts/HCT.sol
+++ src/contracts/HCT.sol
@@ -1,8 +1,8 @@
-pragma solidity >= 0.5.0;
+pragma solidity >= 0.4.0 < 0.6.4;
 
-import "./ERC20.sol";
+import "hct/token/ERC20.sol";
 
-contract HCT is ERC20 {
+contract HCTToken is ERC20 {
    
     mapping (address => uint256) private _balances;
     mapping (address => mapping (address => uint256)) private _allowances;
@@ -14,12 +14,14 @@ contract HCT is ERC20 {
     uint256 tokenPerEther = 10;
 
      constructor () public {
-        _name = "HandiCraftToken";
+        _name = "Handicraft Token";
         _symbol = "HCT";
-        _decimals = 3;
+        _decimals = 0;
+        _totalSupply = 1000000000;
+        _balances[msg.sender] = _totalSupply;
     }
     
-     function  name() public view returns (string memory) {
+    function name() public override view returns (string memory) {
         return _name;
     }
 
@@ -31,15 +33,15 @@ contract HCT is ERC20 {
         return _decimals;
     }
     
-    function totalSupply() public view returns (uint256) {
+    function totalSupply() public override view returns (uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
+    function balanceOf(address account) public override view returns (uint256) {
         return _balances[account];
     }
 
-    function transfer(address recipient, uint256 amount) public returns (bool) {
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
          require(recipient != address(0), "transfer to the zero address");
        //  require(_balances[msg.sender] >= amount);
         _balances[msg.sender] = _balances[msg.sender] - amount;
@@ -47,11 +49,11 @@ contract HCT is ERC20 {
         emit Transfer(msg.sender, recipient, amount);
     }
     
-     function allowance(address owner, address spender) public view returns (uint256) {
+    function allowance(address owner, address spender) public override view returns (uint256) {
         return _allowances[owner][spender];
     }
     
-    function approve(address spender, uint256 amount) public returns (bool) {        // msg.sender = owner
+    function approve(address spender, uint256 amount) public override returns (bool) {        // msg.sender = owner
         require(msg.sender != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
@@ -60,14 +62,14 @@ contract HCT is ERC20 {
         return true;
     }
     
-    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
      //   require(_balances[sender] >= amount && _allowances[sender][msg.sender] >= amount && amount > 0);
-        
+        _allowances[sender][msg.sender] = _allowances[sender][msg.sender] - amount;
         _balances[sender] = _balances[sender] - amount;
         _balances[recipient] = _balances[recipient] + amount;
-        _allowances[sender][msg.sender] = _allowances[sender][msg.sender] - amount;
+        
         
         emit Transfer(sender,recipient,amount);
         return true;
